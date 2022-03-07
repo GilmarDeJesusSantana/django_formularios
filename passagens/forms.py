f327a517bf23cbf8501b77c50abfba644e109d38
Gilmar <gilmar.jesus@gmail.com>
2022-03-06 22:26:53 -0300

    Melhorado a aparencia do formulario com o widget tweaks.

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,5 +1,8 @@
-from django import  forms
+from django import forms
+from django.forms import Textarea
 from tempus_dominus.widgets import DatePicker
+from datetime import date
+from passagens.classes_viagem import tipos_de_classe
 
 
 class PassagensForms(forms.Form):
@@ -7,3 +10,12 @@ class PassagensForms(forms.Form):
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=date.today())
+    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras ',
+        max_length=200,
+        widget=Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='E-mail',max_length=200)
